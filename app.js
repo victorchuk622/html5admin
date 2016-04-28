1719176810ba64d4ccce231d0536ccc5f8e5a9b7
victorchuk622 <victorchuk622@gmail.com>
2016-04-28 17:12:29 +0800

    static fix 3

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
 //app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
