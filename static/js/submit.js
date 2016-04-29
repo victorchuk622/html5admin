6b4351a34bdd3957743b0e93650028164566c80c
victorchuk622 <victorchuk622@gmail.com>
2016-04-29 17:55:28 +0800

    ejs account linked with data

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,4 +1,8 @@
+
+
 $('document').ready(function () {
+    $.getScript("js/js.cookie.js", function(){ });
+
     $("#login-form").submit(function (event) {
         event.preventDefault();
         var data = $( this).serialize();
@@ -13,8 +17,10 @@ $('document').ready(function () {
                 if (response.success) {
                     console.log(response.success);
                     console.log(response.token);
+                    Cookies.set('token', response.token);
                     localStorage.setItem('token',response.token);
-                    window.location.href="/admin?token="+response.token;
+                    //window.location.href="/admin?token="+response.token;
+                    window.location.href="/admin";
 
 
 
