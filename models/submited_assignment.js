174bf340066764e8e793a30ead798711e3734688
victorchuk622 <victorchuk622@gmail.com>
2016-04-30 18:55:56 +0800

    submit assignment

--- models/submited_assignment.js
+++ models/submited_assignment.js
@@ -3,8 +3,9 @@ var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('Submitted_Assignment', new Schema({
     id: {type: String, required: true},
-    userID: {type: String, required: true}
-
-
-
+    userID: {type: String, required: true},
+    ans: [{
+        questionNo:{type: String, required: true},
+        answer:{type: String, required: true},
+    }]
 }));
