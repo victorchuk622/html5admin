3bbd6dd1f29600d93e3a9ec797194b47f69758e1
victorchuk622 <victorchuk622@gmail.com>
2016-05-03 17:11:59 +0800

    assingment fixing

--- routes/assignments.js
+++ routes/assignments.js
@@ -41,7 +41,27 @@ router.get('/getAssignments', (req, res) => {
             assignment.content=concateContent;
             console.log(assignment.content);
         });
-        res.json(assignments);
+        //res.json(assignments);
+        res.json(
+            [{
+                title: 'Assignment 2',
+                id:'ass2',
+                done: false,
+                create: '2016-03-16T08:23:45.079Z',
+                deadline: '2017-03-16T08:23:45.079Z',
+                info:'This is Assignment 2',
+                content: 'mc$In HTML5, contextmenu and spellcheck are:$$HTML attributes|*Event attributes|Style attributes|HTML elements&oc$In HTML5, which method is used to get the current location of a user?$$getUserPosition()|getPosition()|*getCurrentPosition()&fitb$_____ is used to specify a header for a document or section?$$*<header>'
+
+            },{
+                title: 'Assignment 1',
+                id:'ass1',
+                done: true,
+                create: '2016-03-15T08:23:45.079Z',
+                deadline: '2017-03-15T08:23:45.079Z',
+                info:'This is Assignment 1',
+                content: 'mc$In HTML5, contextmenu and spellcheck are:$$HTML attributes|*Event attributes|Style attributes|HTML elements&oc$In HTML5, which method is used to get the current location of a user?$$getUserPosition()|getPosition()|*getCurrentPosition()&fitb$_____ is used to specify a header for a document or section?$$*<header>'
+            }]
+        );
     });
 });
 
