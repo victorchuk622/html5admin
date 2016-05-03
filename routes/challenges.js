309e12835c763d8e53fde2a81d73fd476d934580
victorchuk622 <victorchuk622@gmail.com>
2016-05-03 19:39:13 +0800

    getChallenges fake data updated 2

--- routes/challenges.js
+++ routes/challenges.js
@@ -19,17 +19,17 @@ router.post('/addChallenge',(req, res) => {
 });
 
 router.get('/getChallenges/round/:round',(req, res) => {
-    res.json([{"teamID": "2016S101",
+    res.json([{"questionID": "2016S101",
         "teamName": "teamKAV",
         "date": "2016-03-16T08:23:45.079Z",
         "done":false,
         "content":"mc$In HTML5, contextmenu and spellcheck are:$$HTML attributes|*Event attributes|Style attributes|HTML elements&oc$In HTML5, which method is used to get the current location of a user?$$getUserPosition()|getPosition()|*getCurrentPosition()",
-},{"teamID": "2",
+},{"questionID": "2",
         "teamName": "2016S102",
         "date": "2016-03-17T08:23:45.079Z",
         "done":false,
         "content":"mc$In HTML5, contextmenu and spellcheck are:$$HTML attributes|*Event attributes|Style attributes|HTML elements&oc$In HTML5, which method is used to get the current location of a user?$$getUserPosition()|getPosition()|*getCurrentPosition()",
-    },{"teamID": "3",
+    },{"questionID": "3",
         "teamName": "2016S103",
         "date": "2016-03-17T08:23:45.079Z",
         "done":false,
@@ -52,71 +52,4 @@ router.get('/addChallenge/:teamID', (req, res) => {
     res.render('addChallenge',{teamID:req.params.teamID});
 });
 
-
-
-
-
-/*
-router.post('/postRecord', function (req, res, next) {
-    //var token = req.body.token;
-    if (true) {
-        var cha = new Challenge(
-            {
-                'questionID': req.body.questionID,
-                'teamID': req.body.teamID,
-                'score': req.body.score
-            });
-        cha.save(function (err) {
-            if (err)
-                res.json({success: false});
-            else
-                res.json({success: true});
-        });
-    }
-});
-*/
-//get single record by _id
-router.get('/getRecord/:cid', function (req, res, next) {
-    var token = req.query.token;
-    // decode token
-    if (token) {
-        // verifies secret and checks exp
-        jwt.verify(token, "test", function (err, decoded) {
-            if (err) {
-                return res.json({success: false, message: 'Authentication failed.'});
-            }
-            else {
-                var payload = jwt.decode(token, "test");
-                console.log(payload.id);
-
-                //var criteria = {};
-                //criteria["_id"] = req.params.qid;
-                //Message.find({criteria},function(err,results){
-
-                //if fail, replace the follow row by above three rows
-                Challenge.find({_id: req.params.cid}, function (err, results) {
-                    if (err)
-                        return res.json({success: false, message: 'Server Error!'});
-
-                    if (results.length > 0)
-                        res.json(results);
-                    else
-                        res.json({message: 'Result not found!'});
-                });
-            }
-        });
-    }
-    else {
-        // return error if there is no token,
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
-});
-
-
-//get by questionID
-
-//get by teamID
 module.exports = router;
