f9d0637760e4dde65467a1719e22be80cdf051a7
victorchuk622 <victorchuk622@gmail.com>
2016-05-06 03:28:12 +0800

    rank fix 3

--- routes/rankings.js
+++ routes/rankings.js
@@ -14,7 +14,7 @@ var Team = require('../models/team')
 router.use(authUser);
 
 router.get('/getRanking/round/:round',(req,res) => {
-    Ranking.find({round:parseInt(req.params.round)}).sort({score: -1}).exec().then((ranks)=> {
+    Ranking.find({round:parseInt(req.params.round)}).sort({score: -1}).lean().exec().then((ranks)=> {
         console.log(ranks);
         for(i=0;i<ranks.length;i++){
             ranks[0].rank = i+1;
@@ -24,7 +24,7 @@ router.get('/getRanking/round/:round',(req,res) => {
 });
 
 router.get('/statistic/round/:round/teamID/:id',(req,res) => {
-    Ranking.find({round:parseInt(req.params.round)}).sort({score: -1}).exec().then((ranks)=>{
+    Ranking.find({round:parseInt(req.params.round)}).sort({score: -1}).lean().exec().then((ranks)=>{
         //console.log(ranks);
         //console.log(req.params.id);
         var target = ranks.filter((val)=> {
